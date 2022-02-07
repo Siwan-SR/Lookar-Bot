72ddda30469c47bb98890c1350a3c3c532de4fc3
Siwan SR <97611483+Siwan-SR@users.noreply.github.com>
2022-02-07 07:50:07 +0000

    New commands

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from replit import db
 import sys
 
 bot_name = 'Lookar'
+discord = 'https://discord.com/'
+github = 'https://github.com/'
+replit = 'https://replit.com/'
 
 print("Hello! I am Lookar Bot my prefix is '!'\n")
 
@@ -19,6 +22,19 @@ elif cmd_wish == '!greet':
 elif cmd_wish == '!trivia':
 
  print("Still under developement")
+
+elif cmd_wish == '!quit':
+  print("You have quited the program.")
+  sys.exit()
+
+elif cmd_wish == '!discord':
+  print("The owner currently doesn't have a public discord server. Here is the link of discord's website: ",discord)
+
+elif cmd_wish == '!github':
+  print("GitHub's website: ",github + ".\nOwner GitHub: https://github.com/@Siwan-SR/")
+
+elif cmd_wish == '!replit':
+  print("Replit's website: ",replit + ".\nOwner Replit: https://replit.com/@Siwan-SR/")
 
 else:
   print("That is not a command! Do !commands to see all commands.")
